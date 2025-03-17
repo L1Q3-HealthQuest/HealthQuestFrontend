6bc648487455d83cac32f05c55da1882081e95db
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-17 18:54:00 +0100

    Added camera movement and restrictions

--- Assets/Scripts/GanzenBord.cs
+++ Assets/Scripts/GanzenBord.cs
@@ -15,19 +15,38 @@ public class Ganzenbord : MonoBehaviour
     private bool appoinmentPopUpActive = true;
     private string[] appointmentTitles = new string[] { "Title 1", "Title 2", "Title 3" };
     private string[] appointmentDescriptions = new string[] { "Description 1", "Description 2", "Description 3" };
-    private float[] cameraPositions = new float[] { 283, 500, 600 };
+    private float[] cameraPositions = new float[] { 283, 500, 700 };
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
         ToggleSelection();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        switch (mainCamera.transform.position.x)
+        {
+            case < 283:
+                mainCamera.transform.position = new Vector3(283, mainCamera.transform.position.y, mainCamera.transform.position.z);
+                return;
+            case > 500:
+                mainCamera.transform.position = new Vector3(500, mainCamera.transform.position.y, mainCamera.transform.position.z);
+                return;
+        }
+
+        // Get the horizontal input (left/right)
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move the camera only along the X-axis (right/left)
+        var movement = mainCamera.transform.right * horizontalInput;
+
+        // Update the camera's position by adding the movement to the current position, but keep the Y and Z unchanged
+        mainCamera.transform.position = new Vector3(mainCamera.transform.position.x + movement.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
         
     }
+
 
     public void OnLevelSelected(int level)
     {
