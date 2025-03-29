109997dee1b5dea99d2a4bb1620f778efdc652c0
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-29 20:03:51 +0100

    Move JSON parsing methods to JsonHelper

--- Assets/Scripts/ApiClient/JsonHelper.cs
+++ Assets/Scripts/ApiClient/JsonHelper.cs
@@ -15,6 +15,90 @@ public static class JsonHelper
     {
         return JsonUtility.FromJson<Token>(data);
     }
+
+    public static IWebRequestReponse ParseGuardianResponse(IWebRequestReponse webRequestResponse)
+    {
+        switch (webRequestResponse)
+        {
+            case WebRequestData<string> data:
+                Debug.Log("Response data raw: " + data.Data); // TODO: remove debug log
+                var guardian = JsonUtility.FromJson<Guardian>(data.Data);
+                WebRequestData<Guardian> parsedWebRequestData = new(guardian);
+                return parsedWebRequestData;
+            default:
+                return webRequestResponse;
+        }
+    }
+
+    public static IWebRequestReponse ParseGuardianListResponse(IWebRequestReponse webRequestResponse)
+    {
+        switch (webRequestResponse)
+        {
+            case WebRequestData<string> data:
+                Debug.Log("Response data raw: " + data.Data); // TODO: remove debug log
+                List<Guardian> guardians = ParseJsonArray<Guardian>(data.Data);
+                WebRequestData<List<Guardian>> parsedWebRequestData = new(guardians);
+                return parsedWebRequestData;
+            default:
+                return webRequestResponse;
+        }
+    }
+
+    public static IWebRequestReponse ParsePatientResponse(IWebRequestReponse webRequestResponse)
+    {
+        switch (webRequestResponse)
+        {
+            case WebRequestData<string> data:
+                Debug.Log("Response data raw: " + data.Data); // TODO: remove debug log
+                var patient = JsonUtility.FromJson<Patient>(data.Data);
+                WebRequestData<Patient> parsedWebRequestData = new(patient);
+                return parsedWebRequestData;
+            default:
+                return webRequestResponse;
+        }
+    }
+
+    public static IWebRequestReponse ParsePatientListResponse(IWebRequestReponse webRequestResponse)
+    {
+        switch (webRequestResponse)
+        {
+            case WebRequestData<string> data:
+                Debug.Log("Response data raw: " + data.Data); // TODO: remove debug log
+                List<Patient> patients = ParseJsonArray<Patient>(data.Data);
+                WebRequestData<List<Patient>> parsedWebRequestData = new(patients);
+                return parsedWebRequestData;
+            default:
+                return webRequestResponse;
+        }
+    }
+
+    public static IWebRequestReponse ParseStickerResponse(IWebRequestReponse webRequestResponse)
+    {
+        switch (webRequestResponse)
+        {
+            case WebRequestData<string> data:
+                Debug.Log("Response data raw: " + data.Data); // TODO: remove debug log
+                Sticker sticker = JsonUtility.FromJson<Sticker>(data.Data);
+                WebRequestData<Sticker> parsedWebRequestData = new(sticker);
+                return parsedWebRequestData;
+            default:
+                return webRequestResponse;
+        }
+    }
+
+    public static IWebRequestReponse ParseStickerListResponse(IWebRequestReponse webRequestResponse)
+    {
+        switch (webRequestResponse)
+        {
+            case WebRequestData<string> data:
+                Debug.Log("Response data raw: " + data.Data); // TODO: remove debug log
+                List<Patient> patients = ParseJsonArray<Patient>(data.Data);
+                WebRequestData<List<Patient>> parsedWebRequestData = new(patients);
+                return parsedWebRequestData;
+            default:
+                return webRequestResponse;
+        }
+    }
 }
 
 [Serializable]
--- Assets/Scripts/ApiClient/ModelApiClients/GuardianApiClient.cs
+++ Assets/Scripts/ApiClient/ModelApiClients/GuardianApiClient.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GuardianApiClient : MonoBehaviour
@@ -12,7 +9,7 @@ public class GuardianApiClient : MonoBehaviour
         string route = "/api/v1/guardian";
 
         IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
-        return ParseGuardianResponse(webRequestResponse);
+        return JsonHelper.ParseGuardianResponse(webRequestResponse);
     }
 
     public async Awaitable<IWebRequestReponse> ReadGuardianById(string guardianId)
@@ -20,7 +17,7 @@ public class GuardianApiClient : MonoBehaviour
         string route = "/api/v1/guardian" + guardianId;
 
         IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
-        return ParseGuardianResponse(webRequestResponse);
+        return JsonHelper.ParseGuardianResponse(webRequestResponse);
     }
 
     public async Awaitable<IWebRequestReponse> CreateGuardian(Guardian guardianData)
@@ -29,51 +26,21 @@ public class GuardianApiClient : MonoBehaviour
         string data = JsonUtility.ToJson(guardianData);
 
         IWebRequestReponse webRequestResponse = await webClient.SendPostRequestAsync(route, data);
-        return ParseGuardianResponse(webRequestResponse);
+        return JsonHelper.ParseGuardianResponse(webRequestResponse);
     }
 
-    //public async Awaitable<IWebRequestReponse> UpdateGuardian(string guardianId, Guardian guardianData)
-    //{
-    //    string route = "/api/v1/guardian/" + guardianId;
-    //    string data = JsonUtility.ToJson(guardianData);
-    //
-    //    IWebRequestReponse webRequestResponse = await webClient.SendPutRequestAsync(route, data);
-    //    return ParseGuardianResponse(webRequestResponse);
-    //}
+    public async Awaitable<IWebRequestReponse> UpdateGuardian(string guardianId, Guardian guardianData)
+    {
+        string route = "/api/v1/guardian/" + guardianId;
+        string data = JsonUtility.ToJson(guardianData);
+
+        IWebRequestReponse webRequestResponse = await webClient.SendPutRequestAsync(route, data);
+        return JsonHelper.ParseGuardianResponse(webRequestResponse);
+    }
 
     public async Awaitable<IWebRequestReponse> DeleteGuardian(string guardianId)
     {
         string route = "/api/v1/guardian/" + guardianId;
         return await webClient.SendDeleteRequestAsync(route);
-    }
-
-
-    // Parse methodes for response data
-    private IWebRequestReponse ParseGuardianResponse(IWebRequestReponse webRequestResponse)
-    {
-        switch (webRequestResponse)
-        {
-            case WebRequestData<string> data:
-                Debug.Log("Response data raw: " + data.Data); // TODO: remove debug log
-                var guardian = JsonUtility.FromJson<Guardian>(data.Data);
-                WebRequestData<Guardian> parsedWebRequestData = new(guardian);
-                return parsedWebRequestData;
-            default:
-                return webRequestResponse;
-        }
-    }
-
-    private IWebRequestReponse ParseGuardianListResponse(IWebRequestReponse webRequestResponse)
-    {
-        switch (webRequestResponse)
-        {
-            case WebRequestData<string> data:
-                Debug.Log("Response data raw: " + data.Data); // TODO: remove debug log
-                List<Guardian> guardians = JsonHelper.ParseJsonArray<Guardian>(data.Data);
-                WebRequestData<List<Guardian>> parsedWebRequestData = new(guardians);
-                return parsedWebRequestData;
-            default:
-                return webRequestResponse;
-        }
     }
 }
