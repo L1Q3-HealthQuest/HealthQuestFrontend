ac54b6e798d852395bb4f5346a603061d893d417
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-29 20:04:32 +0100

    Add PatientApiClient for managing patient CRUD operations

---
+++ Assets/Scripts/ApiClient/ModelApiClients/PatientApiClient.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+class PatientApiClient
+{
+    public WebClient webClient;
+
+    public async Awaitable<IWebRequestReponse> ReadPatientAsync()
+    {
+        string route = "/api/v1/patient";
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParsePatientResponse(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> ReadPatientById(string patientId)
+    {
+        string route = "/api/v1/patient" + patientId;
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParsePatientResponse(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> CreatePatient(Patient patientData)
+    {
+        string route = "/api/v1/patient";
+        string data = JsonUtility.ToJson(patientData);
+
+        IWebRequestReponse webRequestResponse = await webClient.SendPostRequestAsync(route, data);
+        return JsonHelper.ParsePatientResponse(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> UpdatePatient(string patientId, Patient patientData)
+    {
+        string route = "/api/v1/patient/" + patientId;
+        string data = JsonUtility.ToJson(patientData);
+
+        IWebRequestReponse webRequestResponse = await webClient.SendPutRequestAsync(route, data);
+        return JsonHelper.ParsePatientResponse(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> DeletePatient(string patientId)
+    {
+        string route = "/api/v1/patient/" + patientId;
+        return await webClient.SendDeleteRequestAsync(route);
+    }
+}
