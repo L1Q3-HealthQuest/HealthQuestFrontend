1f70ad19588faeb017da545788a32f83497de193
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-29 20:35:14 +0100

    Add async methods for managing patient stickers

--- Assets/Scripts/ApiClient/ModelApiClients/PatientApiClient.cs
+++ Assets/Scripts/ApiClient/ModelApiClients/PatientApiClient.cs
@@ -12,7 +12,7 @@ class PatientApiClient
         return JsonHelper.ParsePatientResponse(webRequestResponse);
     }
 
-    public async Awaitable<IWebRequestReponse> ReadPatientById(string patientId)
+    public async Awaitable<IWebRequestReponse> ReadPatientByIdAsync(string patientId)
     {
         string route = "/api/v1/patient" + patientId;
 
@@ -43,4 +43,29 @@ class PatientApiClient
         string route = "/api/v1/patient/" + patientId;
         return await webClient.SendDeleteRequestAsync(route);
     }
+
+    public async Awaitable<IWebRequestReponse> ReadUnlockedStickersFromPatientAsync(string patientId)
+    {
+        string route = "/api/v1/patient/" + patientId + "/stickers";
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParseStickerListResponse(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> AddUnlockedStickerToPatientAsync(string patientId, Sticker sticker)
+    {
+        string route = "/api/v1/patient/" + patientId + "/stickers";
+        string data = JsonUtility.ToJson(sticker);
+
+        IWebRequestReponse webRequestResponse = await webClient.SendPostRequestAsync(route, data);
+        return JsonHelper.ParseStickerResponse(webRequestResponse);
+    }
+
+    //public async Awaitable<IWebRequestReponse> ReadCompletedAppointmentsFromPatientAsync(string patientId)
+    //{
+    //    string route = "/api/v1/patient/" + patientId + "/completed-appointments";
+
+    //    IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+    //    return JsonHelper.ParseAppointmentResponse(webRequestResponse);
+    //}
 }
