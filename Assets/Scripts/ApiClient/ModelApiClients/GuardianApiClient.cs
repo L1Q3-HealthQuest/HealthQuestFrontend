cc9c3889b806cfe7ab120c0a1615c70d7117fed3
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-29 17:22:17 +0100

    Add Guardian API client methods for CRUD operations

--- Assets/Scripts/ApiClient/ModelApiClients/GuardianApiClient.cs
+++ Assets/Scripts/ApiClient/ModelApiClients/GuardianApiClient.cs
@@ -6,4 +6,74 @@ using UnityEngine;
 public class GuardianApiClient : MonoBehaviour
 {
   public WebClient webClient;
+
+    public async Awaitable<IWebRequestReponse> ReadGuardian()
+    {
+        string route = "/api/v1/guardian";
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return ParseGuardianResponse(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> ReadGuardianById(string guardianId)
+    {
+        string route = "/api/v1/guardian" + guardianId;
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return ParseGuardianResponse(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> CreateGuardian(Guardian guardianData)
+    {
+        string route = "/api/v1/guardian";
+        string data = JsonUtility.ToJson(guardianData);
+
+        IWebRequestReponse webRequestResponse = await webClient.SendPostRequestAsync(route, data);
+        return ParseGuardianResponse(webRequestResponse);
+    }
+
+    //public async Awaitable<IWebRequestReponse> UpdateGuardian(string guardianId, Guardian guardianData)
+    //{
+    //    string route = "/api/v1/guardian/" + guardianId;
+    //    string data = JsonUtility.ToJson(guardianData);
+    //
+    //    IWebRequestReponse webRequestResponse = await webClient.SendPutRequestAsync(route, data);
+    //    return ParseGuardianResponse(webRequestResponse);
+    //}
+
+    public async Awaitable<IWebRequestReponse> DeleteGuardian(string environmentId)
+    {
+        string route = "/api/v1/guardian/" + environmentId;
+        return await webClient.SendDeleteRequestAsync(route);
+    }
+
+
+    // Parse methodes for response data
+    private IWebRequestReponse ParseGuardianResponse(IWebRequestReponse webRequestResponse)
+    {
+        switch (webRequestResponse)
+        {
+            case WebRequestData<string> data:
+                Debug.Log("Response data raw: " + data.Data); // TODO: remove debug log
+                var guardian = JsonUtility.FromJson<Guardian>(data.Data);
+                WebRequestData<Guardian> parsedWebRequestData = new(guardian);
+                return parsedWebRequestData;
+            default:
+                return webRequestResponse;
+        }
+    }
+
+    private IWebRequestReponse ParseGuardianListResponse(IWebRequestReponse webRequestResponse)
+    {
+        switch (webRequestResponse)
+        {
+            case WebRequestData<string> data:
+                Debug.Log("Response data raw: " + data.Data); // TODO: remove debug log
+                List<Guardian> guardians = JsonHelper.ParseJsonArray<Guardian>(data.Data);
+                WebRequestData<List<Guardian>> parsedWebRequestData = new(guardians);
+                return parsedWebRequestData;
+            default:
+                return webRequestResponse;
+        }
+    }
 }
