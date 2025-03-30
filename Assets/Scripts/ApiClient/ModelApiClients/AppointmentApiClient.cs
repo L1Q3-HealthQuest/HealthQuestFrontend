af0ca97d83488f17d7f69bf27263f79577ea1282
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-30 14:14:43 +0200

    Add AppointmentApiClient for managing appointment-related API interactions

---
+++ Assets/Scripts/ApiClient/ModelApiClients/AppointmentApiClient.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class AppointmentApiClient : MonoBehaviour
+{
+    public WebClient webClient;
+
+    public async Awaitable<IWebRequestReponse> ReadAppointmentsAsync()
+    {
+        string route = "/api/v1/appointments";
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParseListResponse<Appointment>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> ReadAppointmentByIdAsync(string appointmentId)
+    {
+        string route = "/api/v1/appointments/" + appointmentId;
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParseResponse<Appointment>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> CreateAppointmentAsync(Appointment appointmentData)
+    {
+        string route = "/api/v1/appointments";
+        string data = JsonUtility.ToJson(appointmentData);
+
+        IWebRequestReponse webRequestResponse = await webClient.SendPostRequestAsync(route, data);
+        return JsonHelper.ParseResponse<Appointment>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> UpdateAppointmentAsync(string appointmentId, Appointment appointmentData)
+    {
+        string route = "/api/v1/appointments/" + appointmentId;
+        string data = JsonUtility.ToJson(appointmentData);
+
+        IWebRequestReponse webRequestResponse = await webClient.SendPutRequestAsync(route, data);
+        return JsonHelper.ParseResponse<Patient>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> DeleteAppointmentAsync(string appointmentId)
+    {
+        string route = "/api/v1/appointments/" + appointmentId;
+        return await webClient.SendDeleteRequestAsync(route);
+    }
+}
