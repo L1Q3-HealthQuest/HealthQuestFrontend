752ca9cea7fb45a87e8e9a73193fb9b8bc2589a2
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-30 14:57:38 +0200

    Add JournalApiClient for managing journal-related API interactions

---
+++ Assets/Scripts/ApiClient/ModelApiClients/JournalApiClient.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+public class JournalApiClient : MonoBehaviour
+{
+    public WebClient webClient;
+
+    public async Awaitable<IWebRequestReponse> ReadJournalEntriesAsync()
+    {
+        string route = "/api/v1/journal";
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParseListResponse<JournalEntry>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> ReadJournalEntriesAsync(string patientId)
+    {
+        string route = $"/api/v1/journal?patientId={patientId}";
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParseListResponse<JournalEntry>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> ReadJournalEntriesAsync(string guardianId, string patientId)
+    {
+        string route = $"/api/v1/journal?guardianId={guardianId}&patientId={patientId}";
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParseListResponse<JournalEntry>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> ReadJournalEntriesByGuardianAsync(string guardianId)
+    {
+        string route = $"/api/v1/journal?guardianId={guardianId}";
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParseListResponse<JournalEntry>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> ReadJournalEntryByIdAsync(string journalEntryId)
+    {
+        string route = "/api/v1/stickers/" + journalEntryId;
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParseResponse<Sticker>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> CreateJournalEntryAsync(JournalEntry journalData)
+    {
+        string route = "/api/v1/stickers";
+        string data = JsonUtility.ToJson(journalData);
+
+        IWebRequestReponse webRequestResponse = await webClient.SendPostRequestAsync(route, data);
+        return JsonHelper.ParseResponse<Sticker>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> UpdateJournalEntryAsync(string journalEntryId, JournalEntry journalData)
+    {
+        string route = "/api/v1/stickers/" + journalEntryId;
+        string data = JsonUtility.ToJson(journalData);
+
+        IWebRequestReponse webRequestResponse = await webClient.SendPutRequestAsync(route, data);
+        return JsonHelper.ParseResponse<Sticker>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> DeleteJournalEntryAsync(string journalEntryId)
+    {
+        string route = "/api/v1/stickers/" + journalEntryId;
+        return await webClient.SendDeleteRequestAsync(route);
+    }
+}
