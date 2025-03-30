7d932aa862e7c6e9ded8c21208f5c5e70aa8a27a
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-30 13:43:36 +0200

    Add async methods for reading and adding appointments in TreatmentApiClient

--- Assets/Scripts/ApiClient/ModelApiClients/TreatmentApiClient.cs
+++ Assets/Scripts/ApiClient/ModelApiClients/TreatmentApiClient.cs
@@ -44,4 +44,21 @@ class TreatmentApiClient
         string route = "/api/v1/treatments/" + treatmentId;
         return await webClient.SendDeleteRequestAsync(route);
     }
+
+    public async Awaitable<IWebRequestReponse> ReadAppointmentsByTreatmentIdAsync(string treatmentId)
+    {
+        string route = "/api/v1/treatments/" + treatmentId + "/appointments";
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParseListResponse<Appointment>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> AddAppointmentToTreatmentAsync(string treatmentId, string appointmentId, int sequence)
+    {
+        string route = "/api/v1/treatments/" + treatmentId + "/appointments/ " + appointmentId + "?sequence=" + sequence;
+        string data = "";
+
+        IWebRequestReponse webRequestResponse = await webClient.SendPostRequestAsync(route, data);
+        return JsonHelper.ParseResponse<Appointment>(webRequestResponse); // TODO: check if it should return a list
+    }
 }
