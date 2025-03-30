70ffa27902a040aa1a5c191e1b9c7dfe23892e1e
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-03-30 13:16:28 +0200

    Add TreatmentApiClient for managing treatment-related API interactions

---
+++ Assets/Scripts/ApiClient/ModelApiClients/TreatmentApiClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+class TreatmentApiClient
+    {
+    public WebClient webClient;
+
+    public async Awaitable<IWebRequestReponse> ReadTreatmentsAsync()
+    {
+        string route = "/api/v1/treatments";
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParseListResponse<Treatment>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> ReadTreatmentByIdAsync(string treatmentId)
+    {
+        string route = "/api/v1/treatments/" + treatmentId;
+
+        IWebRequestReponse webRequestResponse = await webClient.SendGetRequestAsync(route);
+        return JsonHelper.ParseResponse<Treatment>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> CreateTreatmentAsync(Treatment treatmentData)
+    {
+        string route = "/api/v1/treatments";
+        string data = JsonUtility.ToJson(treatmentData);
+
+        IWebRequestReponse webRequestResponse = await webClient.SendPostRequestAsync(route, data);
+        return JsonHelper.ParseResponse<Treatment>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> UpdateTreatmentAsync(string treatmentId, Treatment treatmentData)
+    {
+        string route = "/api/v1/treatments/" + treatmentId;
+        string data = JsonUtility.ToJson(treatmentData);
+
+        IWebRequestReponse webRequestResponse = await webClient.SendPutRequestAsync(route, data);
+        return JsonHelper.ParseResponse<Patient>(webRequestResponse);
+    }
+
+    public async Awaitable<IWebRequestReponse> DeleteTreatmentAsync(string treatmentId)
+    {
+        string route = "/api/v1/treatments/" + treatmentId;
+        return await webClient.SendDeleteRequestAsync(route);
+    }
+}
