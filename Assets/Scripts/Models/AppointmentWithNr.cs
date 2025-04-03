701cabb67a7c57c4871fd565ee6e921d6b902faa
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-04-03 17:26:57 +0200

    Enhance AppointmentWithNr class documentation

--- Assets/Scripts/Models/AppointmentWithNr.cs
+++ Assets/Scripts/Models/AppointmentWithNr.cs
@@ -1,10 +1,14 @@
 using System;
 
+/// <summary>
+/// Represents an appointment with an associated appointment number.
+/// Inherits from the <see cref="Appointment"/> class.
+/// </summary>
 [Serializable]
 public class AppointmentWithNr : Appointment
 {
     /// <summary>
-    /// Gets or sets the appointment number.
+    /// Gets or sets the number representing the position of the appointment within the treatment plan.
     /// </summary>
     public int appointmentNr;
 }
