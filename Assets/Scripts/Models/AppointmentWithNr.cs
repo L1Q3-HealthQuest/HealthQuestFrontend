72248f03d4b3f4984811d9bf16a049c809fa2ba4
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-04-03 15:21:22 +0200

    Rename property AppointmentNr to appointmentNr for Unity JSON parsing

--- Assets/Scripts/Models/AppointmentWithNr.cs
+++ Assets/Scripts/Models/AppointmentWithNr.cs
@@ -3,5 +3,5 @@ public class AppointmentWithNr : Appointment
     /// <summary>
     /// Gets or sets the appointment number.
     /// </summary>
-    public int AppointmentNr { get; set; }
+    public int appointmentNr { get; set; }
 }
